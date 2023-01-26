c17756ca77df96a3266d5c5410c4625a9493a82f
Jukka-Pekka Seppänen <jukka-pekka.seppanen@vincit.fi>
2023-01-26 22:20:45 +0200

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import { App } from '@slack/bolt';
+import { App, ExpressReceiver } from '@slack/bolt';
 import { config } from 'dotenv';
 
 if (process.env.NODE_ENV !== 'production') {
   config();
 }
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+const receiver = new ExpressReceiver({
   signingSecret: process.env.SIGNING_SECRET,
+});
+
+const app = new App({
+  receiver,
+  token: process.env.SLACK_BOT_TOKEN,
 });
 
 app.message(':hr-whistle:', async ({ message, say }) => {
@@ -19,3 +23,7 @@ app.message(':hr-whistle:', async ({ message, say }) => {
   await app.start(process.env.PORT || 4000);
   console.log('⚡️ Bolt app started');
 })();
+
+receiver.router.get('/', (req, res) => {
+  res.send('Hello there!');
+});
